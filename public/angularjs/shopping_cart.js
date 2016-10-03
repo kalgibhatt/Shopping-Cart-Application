d8d87a7c63bd4f26a46879c7a9fb4271eaf598c0
Kalgi Bhatt <kalgibhatt1@gmail.com>
2016-10-02 18:19:54 -0700

    Final Commit

--- public/angularjs/shopping_cart.js
+++ public/angularjs/shopping_cart.js
@@ -1,18 +1,74 @@
 var cart = angular.module('cart', []);
 cart.controller('ShoppingCart', function($scope, $http) {
+	
+	$scope.products = [{
+		"product_name" : "MacBook Air 13.3-Inch Laptop",
+		"product_quantity" : 0,
+		"product_price" : 887.99,
+		"product_image" : "http://www.interestingreality.com/wp-content/uploads/2016/01/apple-laptop-battery-replacement.gif"					
+	},
+	{
+		"product_name" : "Wireless Bluetooth Headphones",
+		"product_quantity" : 0,
+		"product_price" : 15.29,
+		"product_image" : "http://cdn2.greatfon.com/uploads/picture/751/69751/naushniki-kraski-fon.jpg"
+	},
+	{
+		"product_name" : "Makeup Brush Set",
+		"product_quantity" : 0,
+		"product_price" : 11.04,
+		"product_image" : "https://s-media-cache-ak0.pinimg.com/736x/f9/37/0c/f9370cfd63c877a42e7d794224e847bb.jpg"
+	},
+	{
+		"product_name" : "Lemon Squeezer",
+		"product_quantity" : 0,
+		"product_price" : 8.99,
+		"product_image" : "http://mco-d2-p.mlstatic.com/exprimidor-de-limon-chef-prioridad-y-exprimidor-de-limon-700321-MCO20738767706_052016-O.jpg"
+	},
+	{
+		"product_name" : "Tide Detergent",
+		"product_quantity" : 0,
+		"product_price" : 10.67,
+		"product_image" : "http://www.hodhodnews.ir/upload/20160704/70653839581.jpg"
+	}];
+	
+	$http({
+		method : "POST",
+		url : '/fetchCart',
+	}).success(function(data) {
+		$scope.cart = data.cart;
+		$scope.total = data.total;
+	});
 
-	$scope.add = function() {
-		$http({
-			method : "POST",
-			url : '/addtocart',
-			data : {
-				"quantity" : $scope.quantity,
-				"price" : $scope.price,
-				"image" : $scope.image,
-				"name" : $scope.name
+	$scope.addToCart = function() {
+		
+		var cart = [];
+		var negativeQuantity = false;
+		for(var i=0;i<$scope.products.length;i++){
+			if($scope.products[i].product_quantity > 0){
+				cart.push ($scope.products[i]);
+			} else if($scope.products[i].product_quantity < 0) {
+				negativeQuantity = true;
+				break;
 			}
-		}).success(function(data) {
-			$scope.total = data.total;
-		});
+		}
+		
+		if(!negativeQuantity) {
+			$http({
+				method : "POST",
+				url : '/addtocart',
+				data : {
+					"cart" : cart
+				}
+			}).success(function(data) {
+				$scope.cart = data.cart;
+				$scope.total = data.total;
+				for(var i = 0; i < $scope.products.length; i++) {
+					$scope.products[i].product_quantity = 0;
+				}
+			});
+		} else {
+			$scope.errorMessage = "Select Positive values for quantity!";
+		}
 	};
 });
--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,44 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/addtocart',function(req,res,next) {
-	var name = req.body.name;
-	var image = req.body.image;
-	var quantity = Number(req.body.quantity);
-	var price = Number(req.body.price);
-	var total = quantity * price;
+router.post('/fetchCart', function(req, res, next) {
 	res.send({
-			"total": total
+		"cart"	:	req.session.cart,
+		"total"	:	req.session.total
+	});
+});
+
+router.post('/addtocart',function(req,res,next) {
+	var cartItems = req.body.cart;
+	var cart = [];
+	var total = 0;
+	
+	if(req.session !== undefined && req.session.cart !== undefined && req.session.total !== undefined){
+		cart = req.session.cart;
+		total = req.session.total;
+	}
+	
+	for(var i=0;i<cartItems.length;i++){
+		var alreadyAdded = false;
+		for(var j=0;j<cart.length;j++){
+			if(cart[j].product_name === cartItems[i].product_name){
+				cart[j].product_quantity = Number(cart[j].product_quantity) + Number(cartItems[i].product_quantity);
+				alreadyAdded = true;
+				break;
+			}
+		}
+		if(!alreadyAdded){
+			cart.push(cartItems[i]);
+		}
+		total = total + Number(cartItems[i].product_price) * Number(cartItems[i].product_quantity);
+	}
+	
+	req.session.cart = cart;
+	req.session.total = total;
+	
+	res.send({
+			"cart"	:	req.session.cart,
+			"total"	:	req.session.total
 	});
 });
 
